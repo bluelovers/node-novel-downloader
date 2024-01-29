dd1b778ed9f2a8b88b0d403b7f5be1fc8355338f
bluelovers <codelovers@users.sourceforge.net>
2024-01-30 02:35:11 +0800

    .

--- packages/novel-downloader/src/site/demo/base.d.ts
+++ packages/novel-downloader/src/site/demo/base.d.ts
@@ -37,12 +37,12 @@ export declare class NovelSiteDemo extends _NovelSite {
     protected _outputAttach<T = any>(novel: INovel, optionsRuntime: IOptionsRuntime, _cache_: {
         url: URL;
         path_novel: string;
-    }, ...argv: any[]): Promise<void | {
+    }, ...argv: any[]): Promise<void | _NovelSite.IVolume[] | {
         id?: string | number;
         parent?: string | number;
         uuid?: string;
         content: import("../../tree").IRowRoot<{}> | IRowVolume<{}> | IRowChapter<{}>;
-    }[] | _NovelSite.IVolume[]>;
+    }[]>;
     protected _processNovel<T = any>(novel: INovel, optionsRuntime: IOptionsRuntime, _cache_: {
         url: URL;
         path_novel: string;
